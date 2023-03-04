205489a331a7e602ddca3662d26db8bbf1d64090
IvanI <sumin.ivan01@mil.ru>
2023-03-04 15:53:52 +1000

    creating and running a bot

---
+++ bot_telegram.py
@@ -0,0 +1,9 @@
+from aiogram.utils import executor
+from create_bot import dp
+from handlers import client
+
+
+if __name__ == '__main__':
+    async def on_start_bot(_):
+        print('Bot started')
+    executor.start_polling(dp, skip_updates=True, on_startup=on_start_bot)
